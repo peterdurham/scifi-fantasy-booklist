65164df4b6533ce0aaa7ac6fbdb71f060b284761
Peter Durham <peterdurham@protonmail.com>
2018-10-10 20:23:23 -0400

    Updates

---
+++ precache-manifest.5c8402bd464b7c96094f93c87c720ed9.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ae44ed04d6865602b1a75b89d4fa86a",
+    "url": "/scifi-fantasy-booklist/static/media/handmaidstale.5ae44ed0.jpg"
+  },
+  {
+    "revision": "c5ec6d4efd113119da8c",
+    "url": "/scifi-fantasy-booklist/static/css/main.379b2adc.chunk.css"
+  },
+  {
+    "revision": "bdeaedf82a96de0bfb62",
+    "url": "/scifi-fantasy-booklist/static/js/1.bdeaedf8.chunk.js"
+  },
+  {
+    "revision": "dfbdab0140d2e264b5cf",
+    "url": "/scifi-fantasy-booklist/static/js/runtime~main.dfbdab01.js"
+  },
+  {
+    "revision": "bf4ab0c94b2ccc129cf8e4e347583f2b",
+    "url": "/scifi-fantasy-booklist/static/media/20000leaguesunderthesea.bf4ab0c9.jpg"
+  },
+  {
+    "revision": "474bf5539b609b3fba9a762db73f6ccb",
+    "url": "/scifi-fantasy-booklist/static/media/2001.474bf553.jpg"
+  },
+  {
+    "revision": "2d1f0c43085b8575767c5eec092b7bd0",
+    "url": "/scifi-fantasy-booklist/static/media/americangods.2d1f0c43.jpg"
+  },
+  {
+    "revision": "94006c298ad59466226bc4c4e7997f11",
+    "url": "/scifi-fantasy-booklist/static/media/anathem.94006c29.jpg"
+  },
+  {
+    "revision": "7af3277fada2b75606fa28e43a4cb366",
+    "url": "/scifi-fantasy-booklist/static/media/assassinsapprentice.7af3277f.jpg"
+  },
+  {
+    "revision": "233114f0f7baac8c66523ec284a2db27",
+    "url": "/scifi-fantasy-booklist/static/media/bookofthenewsun.233114f0.jpg"
+  },
+  {
+    "revision": "17b078e9b3164a3f622381b12cd49c61",
+    "url": "/scifi-fantasy-booklist/static/media/bravenewworld.17b078e9.jpg"
+  },
+  {
+    "revision": "d6bf9a3904a1d1487497072e74254171",
+    "url": "/scifi-fantasy-booklist/static/media/catscradle.d6bf9a39.jpg"
+  },
+  {
+    "revision": "7453089f1410eda1cc721f5fc526f0b2",
+    "url": "/scifi-fantasy-booklist/static/media/childhoodsend.7453089f.jpg"
+  },
+  {
+    "revision": "3e870151861e6c9587b63d5f89dc2479",
+    "url": "/scifi-fantasy-booklist/static/media/clockworkorange.3e870151.jpg"
+  },
+  {
+    "revision": "5b18d18e80324884375836cc2f7e1107",
+    "url": "/scifi-fantasy-booklist/static/media/contact.5b18d18e.jpg"
+  },
+  {
+    "revision": "d30c796601274b61baf1757d06be0edf",
+    "url": "/scifi-fantasy-booklist/static/media/cryptonomicon.d30c7966.jpg"
+  },
+  {
+    "revision": "a4b2e9d101230ad8289552b8246026be",
+    "url": "/scifi-fantasy-booklist/static/media/doandroidsdreamofelectricsheep.a4b2e9d1.jpg"
+  },
+  {
+    "revision": "37023355e54e1ce38f7b3260a98249f2",
+    "url": "/scifi-fantasy-booklist/static/media/dragonflight.37023355.jpg"
+  },
+  {
+    "revision": "ba602fdf8da059f06ad0e396d2516c62",
+    "url": "/scifi-fantasy-booklist/static/media/dune.ba602fdf.jpg"
+  },
+  {
+    "revision": "1177726bf81362f6c438abaf9a5cd163",
+    "url": "/scifi-fantasy-booklist/static/media/endersgame.1177726b.jpg"
+  },
+  {
+    "revision": "5679ef0ecfa9ac54355d36a53973bad6",
+    "url": "/scifi-fantasy-booklist/static/media/eyeoftheworld.5679ef0e.jpg"
+  },
+  {
+    "revision": "1ab6728bbb4ec621ca490f5ab4c56bf7",
+    "url": "/scifi-fantasy-booklist/static/media/foundation.1ab6728b.jpg"
+  },
+  {
+    "revision": "96db984ab1312533bd89d4ce5baff922",
+    "url": "/scifi-fantasy-booklist/static/media/furiesofcalderon.96db984a.jpg"
+  },
+  {
+    "revision": "f818cf53025a403b9049b587e1402b26",
+    "url": "/scifi-fantasy-booklist/static/media/gameofthrones.f818cf53.jpg"
+  },
+  {
+    "revision": "91f45562e4e4da7eb2993e02b95ce370",
+    "url": "/scifi-fantasy-booklist/static/media/gardensofthemoon.91f45562.jpg"
+  },
+  {
+    "revision": "1ac1627d7ef96180a782a081c21b24ac",
+    "url": "/scifi-fantasy-booklist/static/media/gunslinger.1ac1627d.jpg"
+  },
+  {
+    "revision": "c5ec6d4efd113119da8c",
+    "url": "/scifi-fantasy-booklist/static/js/main.c5ec6d4e.chunk.js"
+  },
+  {
+    "revision": "6c288dd327c23e9c0f63443659ce8089",
+    "url": "/scifi-fantasy-booklist/static/media/hitchhikersguidetothegalaxy.6c288dd3.jpg"
+  },
+  {
+    "revision": "92d4550f31c5b28d75c51404d9bcdb91",
+    "url": "/scifi-fantasy-booklist/static/media/homeland.92d4550f.jpg"
+  },
+  {
+    "revision": "8dd47c4cbce1e1c9ec1dafd86d6d42ca",
+    "url": "/scifi-fantasy-booklist/static/media/lastunicorn.8dd47c4c.jpg"
+  },
+  {
+    "revision": "4f6aafce5718ddfbb25e5658ec578a3d",
+    "url": "/scifi-fantasy-booklist/static/media/lordoftherings.4f6aafce.jpg"
+  },
+  {
+    "revision": "11c53f5d93e0bfa3d60d7afcff36f398",
+    "url": "/scifi-fantasy-booklist/static/media/martianchronicles.11c53f5d.jpg"
+  },
+  {
+    "revision": "a8f3569d73b029d458f3cceb32011c8d",
+    "url": "/scifi-fantasy-booklist/static/media/mistborn.a8f3569d.jpg"
+  },
+  {
+    "revision": "bddbfc4731455569fa176c74cbd89673",
+    "url": "/scifi-fantasy-booklist/static/media/mistsofavalon.bddbfc47.jpg"
+  },
+  {
+    "revision": "36cf4bcce84e57f659215689c1115203",
+    "url": "/scifi-fantasy-booklist/static/media/moonisaharshmistress.36cf4bcc.jpg"
+  },
+  {
+    "revision": "ae1fb742c9126ee0547d85ffc6488b51",
+    "url": "/scifi-fantasy-booklist/static/media/nameofthewind.ae1fb742.jpg"
+  },
+  {
+    "revision": "8e412061ba9fe22acb3c30abbb97a469",
+    "url": "/scifi-fantasy-booklist/static/media/neuromancer.8e412061.jpg"
+  },
+  {
+    "revision": "b4b7bfda2720064378b443e2aa21c59b",
+    "url": "/scifi-fantasy-booklist/static/media/neverwhere.b4b7bfda.jpg"
+  },
+  {
+    "revision": "f755f22d6b269dd4bda0e1bd0806e04b",
+    "url": "/scifi-fantasy-booklist/static/media/nineteeneightyfour.f755f22d.jpg"
+  },
+  {
+    "revision": "c61d0e0db02f220055a4ce49fa53ebfa",
+    "url": "/scifi-fantasy-booklist/static/media/onceandfutureking.c61d0e0d.jpg"
+  },
+  {
+    "revision": "6975546ec0e74dd98fea977c31c2b868",
+    "url": "/scifi-fantasy-booklist/static/media/princessbride.6975546e.jpg"
+  },
+  {
+    "revision": "5da257b44f1bc1bee59e46eb745eb107",
+    "url": "/scifi-fantasy-booklist/static/media/rendezvouswithrama.5da257b4.jpg"
+  },
+  {
+    "revision": "9944eb300e9f0e0e8bebabdedb5c03f3",
+    "url": "/scifi-fantasy-booklist/static/media/ringworld.9944eb30.jpg"
+  },
+  {
+    "revision": "2614afb3f5f3acd47f1137a77e8c3895",
+    "url": "/scifi-fantasy-booklist/static/media/silmarillion.2614afb3.jpg"
+  },
+  {
+    "revision": "8bc50ef27415ad64bca5cf3c592463f8",
+    "url": "/scifi-fantasy-booklist/static/media/slaughterhousefive.8bc50ef2.jpg"
+  },
+  {
+    "revision": "b03d4df2eb967455484a172915190ad1",
+    "url": "/scifi-fantasy-booklist/static/media/snowcrash.b03d4df2.jpg"
+  },
+  {
+    "revision": "dd1c266276365b51df7b2c15822548bd",
+    "url": "/scifi-fantasy-booklist/static/media/starshiptroopers.dd1c2662.jpg"
+  },
+  {
+    "revision": "49d16260938ebcde4a00460f16ba2055",
+    "url": "/scifi-fantasy-booklist/static/media/strangerinastrangeland.49d16260.jpg"
+  },
+  {
+    "revision": "04fbb9db76a81a097814463693015a53",
+    "url": "/scifi-fantasy-booklist/static/media/timemachine.04fbb9db.jpg"
+  },
+  {
+    "revision": "cd73a0cea8da7357bff7fa5b272cc546",
+    "url": "/scifi-fantasy-booklist/static/media/waroftheworlds.cd73a0ce.jpg"
+  },
+  {
+    "revision": "e9c1dee278132535e179a3329d97ae8f",
+    "url": "/scifi-fantasy-booklist/static/media/watershipdown.e9c1dee2.jpg"
+  },
+  {
+    "revision": "4322d801bd735b2cf104e0b0642484bc",
+    "url": "/scifi-fantasy-booklist/static/media/wayofkings.4322d801.jpg"
+  },
+  {
+    "revision": "21228185966cd389ab5761089ba23570",
+    "url": "/scifi-fantasy-booklist/index.html"
+  }
+];
